f1e50008c1d911e8a897998610475d4f9c58965f
ishotjr <github@ishotjr.com>
2017-12-31 02:50:56 -0500

    :pencil2: :art: Add graphics

--- hello.c
+++ hello.c
@@ -19,5 +19,20 @@ void main()
 	printf("Hello\nDMG!!!");
 	printf("\nPress Start");
 	waitpad(J_START);  // other keys are J_A, J_UP, J_SELECT, etc.
-	printf("\nyay!");
+
+    // draw face
+    color(LTGREY,LTGREY,SOLID);
+    circle(SCREENWIDTH/2,SCREENHEIGHT/2,SCREENHEIGHT/2,M_FILL);
+
+    // eyes
+    color(WHITE,WHITE,SOLID);
+    circle(SCREENWIDTH/3,SCREENHEIGHT/3,SCREENHEIGHT/9,M_FILL);
+    circle(2*SCREENWIDTH/3,SCREENHEIGHT/3,SCREENHEIGHT/9,M_FILL);
+    color(DKGREY,DKGREY,SOLID);
+    circle(SCREENWIDTH/3,17*SCREENHEIGHT/48,SCREENHEIGHT/12,M_FILL);
+    circle(2*SCREENWIDTH/3,17*SCREENHEIGHT/48,SCREENHEIGHT/12,M_FILL);
+
+    // mouth
+    color(DKGREY,DKGREY,SOLID);
+    circle(SCREENWIDTH/2,2*SCREENHEIGHT/3,SCREENHEIGHT/6,M_FILL);
 }
